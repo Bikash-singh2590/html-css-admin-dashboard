2b7f4bf75a1cc9daf8531a8f11e004d49b3fa331
Bikash kumar singh <73824647+Bikash-singh2590@users.noreply.github.com>
2022-08-16 18:01:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const sidebar = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+menuBtn.addEventListener("click", () => {
+  sidebar.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.style.display = "none";
+});
+
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+});
